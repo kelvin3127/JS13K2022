07c643ba2df9997c1c1d30c18c36e335e9808167
kelvin3127 <46061373+kelvin3127@users.noreply.github.com>
2022-08-07 16:33:26 -0400

    Add files via upload

---
+++ Ray Casting Pt 2/sketch.js
@@ -0,0 +1,22 @@
+
+let wall;
+let ray;
+
+function setup() {
+    createCanvas(400, 400);
+    wall = new Boundary(300, 100, 300, 300);
+    ray = new Ray(100, 200);
+}
+
+function draw() {
+    background(0);
+    wall.show();
+    ray.show()
+
+    let pt = ray.cast(wall);
+    console.log(pt);
+    // if (pt) {
+    //     fill(255);
+    //     ellipse(pt.x, pt.y, 8, 8);
+    //}
+}
