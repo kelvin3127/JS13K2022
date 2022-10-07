8a890c39a683533c19ed53fd1a7daf4837a21a3d
kelvin3127 <kelvin3127@users.noreply.github.com>
2022-10-07 14:43:32 -0400

    Update gun & player
    import files prom player into gun

--- source/gun.js
+++ source/gun.js
@@ -3,16 +3,83 @@ import {rotatePoint} from './util.js';
 
 export default class Gun {
     constructor(x,y) {
-        this.gun_nwX = x;
-        this.gun_nwY = y;
-        this.gun_swX = x;
-        this.gun_swY = y;
-        this.gun_neX = x;
-        this.gun_neY = y;
-        this.gun_seX = x;
-        this.gun_seY = y;
+        this.nwX = x;
+        this.nwY = y;
+        this.swX = x;
+        this.swY = y;
+        this.neX = x;
+        this.neY = y;
+        this.seX = x;
+        this.seY = y;
+
+        //hitbox gun data
+        this.gunnwX = x;
+        this.gunnwY = y;
+        this.gunswX = x;
+        this.gunswY = y;
+        this.gunneX = x;
+        this.gunneY = y;
+        this.gunseX = x;
+        this.gunseY = y;
     }
-    update(){}
-    draw() {}
+    update(){
+
+        // if not reloading
+        if (game.mouse.pressed && this.recoil <= 0) {
+        game.bulletManager.addProjectile(new Projectile(this.gunswX,this.gunswY,this.gunRadian));
+        
+        this.recoil = this.fireRate;
+        //let lastBullet = game.bulletManager.clip[game.bulletManager.clip.length-1];
+    }
+
+        const g1 = rotatePoint(this.x, this.y, this.radian, this.x+20, this.y);
+        const g2 = rotatePoint(this.x, this.y, this.radian, this.x+40, this.y - 2);
+        const g3 = rotatePoint(this.x, this.y, this.radian, this.x+40, this.y + 2);
+        const g4 = rotatePoint(this.x, this.y, this.radian, this.x+20, this.y + 2);
+
+        this.nwX = g1.x;
+        this.nwY = g1.y;
+        this.swX = g2.x;
+        this.swY = g2.y;
+        this.neX = g3.x;
+        this.neY = g3.y;
+        this.seX = g4.x;
+        this.seY = g4.y;
+
+        const g5 = rotatePoint(this.x, this.y, this.radian, this.x+12, this.y -4);
+        const g6 = rotatePoint(this.x, this.y, this.radian, this.x+27.5, this.y - 4);
+        const g7 = rotatePoint(this.x, this.y, this.radian, this.x+27.5, this.y + 4);
+        const g8 = rotatePoint(this.x, this.y, this.radian, this.x+12, this.y + 4);
+        
+        this.gunnwX = g5.x;
+        this.gunnwY = g5.y;
+        this.gunswX = g6.x;
+        this.gunswY = g6.y;
+        this.gunneX = g7.x;
+        this.gunneY = g7.y;
+        this.gunseX = g8.x;
+        this.gunseY = g8.y;
+    }
+    draw() {
+
+      //Gun
+      game.context.fillStyle = '#929292';
+      game.context.beginPath()      
+      game.context.moveTo(this.g1.x, this.g1.y);
+      game.context.lineTo(this.g2.x, this.g2.y);      
+      game.context.lineTo(this.g3.x, this.g3.y);      
+      game.context.lineTo(this.g4.x, this.g4.y)
+      game.context.stroke();
+      game.context.fill();
+
+      game.context.fillStyle = '#929292';
+      game.context.beginPath()      
+      game.context.moveTo(this.g5.x, this.g5.y);      
+      game.context.lineTo(this.g6.x, this.g6.y);     
+      game.context.lineTo(this.g7.x, this.g7.y);     
+      game.context.lineTo(this.g8.x, this.g8.y)
+      game.context.stroke();
+      game.context.fill(); 
+    }
     
 }
--- source/player.js
+++ source/player.js
@@ -5,7 +5,8 @@ import {rotatePoint} from './util.js';
 export default class Player {
 
     constructor( x, y ) {
-
+    
+    this.gun = new Gun();
     this.x = x;
     this.y = y;
     this.vx = 0;
@@ -21,18 +22,6 @@ export default class Player {
     this.neY = y;
     this.seX = x;
     this.seY = y; 
-
-    //hitbox gun data
-    this.gunnwX = x;
-    this.gunnwY = y;
-    this.gunswX = x;
-    this.gunswY = y;
-    this.gunneX = x;
-    this.gunneY = y;
-    this.gunseX = x;
-    this.gunseY = y;
-
-    
 
     this.radian = 0
     this.radianTest = 0;
@@ -137,37 +126,6 @@ export default class Player {
       // reset colliding
       this.colliding = false;
 
-      // update hitbox data
-      //Hitbox data
-      const h1 = rotatePoint(this.x, this.y, this.radian, this.x - 10, this.y - 18);
-      const h2 = rotatePoint(this.x, this.y, this.radian, this.x + 12, this.y - 18);
-      const h3 = rotatePoint(this.x, this.y, this.radian, this.x + 12, this.y + 18);
-      const h4 = rotatePoint(this.x, this.y, this.radian, this.x - 10, this.y + 18);
-
-      this.nwX = h1.x;
-      this.nwY = h1.y;
-      this.swX = h2.x;
-      this.swY = h2.y;
-      this.neX = h3.x;
-      this.neY = h3.y;
-      this.seX = h4.x;
-      this.seY = h4.y; 
-    
-      //update hitbox gun data
-      const h5 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y - 3);
-      const h6 = rotatePoint(this.x, this.y, this.radian, this.x + 40, this.y - 3);
-      const h7 = rotatePoint(this.x, this.y, this.radian, this.x + 40, this.y + 3);
-      const h8 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y + 3);
-
-      this.gunRadian = h6.r;
-      this.gunnwX = h5.x;
-      this.gunnwY = h5.y;
-      this.gunswX = h6.x;
-      this.gunswY = h6.y;
-      this.gunneX = h7.x;
-      this.gunneY = h7.y;
-      this.gunseX = h8.x;
-      this.gunseY = h8.y;
 
       // if not reloading
       if (game.mouse.pressed && this.recoil <= 0) {
@@ -206,32 +164,32 @@ export default class Player {
       game.context.lineTo(a4.x, a4.y)
       game.context.fill(); 
 
-      //Gun
-      game.context.fillStyle = '#929292';
-      game.context.beginPath()
-      const g1 = rotatePoint(this.x, this.y, this.radian, this.x+20, this.y);
-      game.context.moveTo(g1.x, g1.y);
-      const g2 = rotatePoint(this.x, this.y, this.radian, this.x+40, this.y - 2);
-      game.context.lineTo(g2.x, g2.y);
-      const g3 = rotatePoint(this.x, this.y, this.radian, this.x+40, this.y + 2);
-      game.context.lineTo(g3.x, g3.y);
-      const g4 = rotatePoint(this.x, this.y, this.radian, this.x+20, this.y + 2);
-      game.context.lineTo(g4.x, g4.y)
-      game.context.stroke();
-      game.context.fill();
+      // //Gun
+      // game.context.fillStyle = '#929292';
+      // game.context.beginPath()
+      // const g1 = rotatePoint(this.x, this.y, this.radian, this.x+20, this.y);
+      // game.context.moveTo(g1.x, g1.y);
+      // const g2 = rotatePoint(this.x, this.y, this.radian, this.x+40, this.y - 2);
+      // game.context.lineTo(g2.x, g2.y);
+      // const g3 = rotatePoint(this.x, this.y, this.radian, this.x+40, this.y + 2);
+      // game.context.lineTo(g3.x, g3.y);
+      // const g4 = rotatePoint(this.x, this.y, this.radian, this.x+20, this.y + 2);
+      // game.context.lineTo(g4.x, g4.y)
+      // game.context.stroke();
+      // game.context.fill();
 
-      game.context.fillStyle = '#929292';
-      game.context.beginPath()
-      const g5 = rotatePoint(this.x, this.y, this.radian, this.x+12, this.y -4);
-      game.context.moveTo(g5.x, g5.y);
-      const g6 = rotatePoint(this.x, this.y, this.radian, this.x+27.5, this.y - 4);
-      game.context.lineTo(g6.x, g6.y);
-      const g7 = rotatePoint(this.x, this.y, this.radian, this.x+27.5, this.y + 4);
-      game.context.lineTo(g7.x, g7.y);
-      const g8 = rotatePoint(this.x, this.y, this.radian, this.x+12, this.y + 4);
-      game.context.lineTo(g8.x, g8.y)
-      game.context.stroke();
-      game.context.fill(); 
+      // game.context.fillStyle = '#929292';
+      // game.context.beginPath()
+      // const g5 = rotatePoint(this.x, this.y, this.radian, this.x+12, this.y -4);
+      // game.context.moveTo(g5.x, g5.y);
+      // const g6 = rotatePoint(this.x, this.y, this.radian, this.x+27.5, this.y - 4);
+      // game.context.lineTo(g6.x, g6.y);
+      // const g7 = rotatePoint(this.x, this.y, this.radian, this.x+27.5, this.y + 4);
+      // game.context.lineTo(g7.x, g7.y);
+      // const g8 = rotatePoint(this.x, this.y, this.radian, this.x+12, this.y + 4);
+      // game.context.lineTo(g8.x, g8.y)
+      // game.context.stroke();
+      // game.context.fill(); 
 
       //Left hand
       game.context.fillStyle = '#C02942';
@@ -284,32 +242,6 @@ export default class Player {
       game.context.beginPath();
       game.context.arc(right.x, right.y, this.radius * 0.1, 0, 2*Math.PI );
       game.context.fill();
-
-
-      //Hit Box
-      game.context.strokeStyle = 'black';
-      game.context.beginPath()
-      game.context.moveTo(this.nwX, this.nwY);
-      game.context.lineTo(this.swX, this.swY);
-      game.context.lineTo(this.neX, this.neY);
-      game.context.lineTo(this.seX, this.seY);
-      game.context.lineTo(this.nwX, this.nwY);
-      game.context.stroke();
-
-      //Gun Hitbox
-      game.context.strokeStyle = 'black';
-      game.context.beginPath()
-      const h5 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y - 3);
-      game.context.moveTo(h5.x, h5.y);
-      const h6 = rotatePoint(this.x, this.y, this.radian, this.x + 40, this.y - 3);
-      game.context.lineTo(h6.x, h6.y);
-      const h7 = rotatePoint(this.x, this.y, this.radian, this.x + 40, this.y + 3);
-      game.context.lineTo(h7.x, h7.y);
-      const h8 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y + 3);
-      game.context.lineTo(h8.x, h8.y);
-      game.context.lineTo(h5.x, h5.y);
-      game.context.stroke();
-
       
     }
   }
