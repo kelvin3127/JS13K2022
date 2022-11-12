ca5e4ec96eb9511d10a07c88669b441f729376f6
kelvin3127 <c.kelvin3127@gmail.com>
2022-11-12 18:35:46 -0500

    Update player.js

--- source/player.js
+++ source/player.js
@@ -25,8 +25,11 @@ export default class Player {
         "dead"
       ];
 
+<<<<<<< Updated upstream
     this.currState = "static"
 
+=======
+>>>>>>> Stashed changes
       //Weapon data
       this.ammo = 0;
       this.bulletClip = [];
@@ -72,8 +75,11 @@ export default class Player {
     update(game) {
       const {keyboard, mouse} = game;
       const {width, height} = game;
+<<<<<<< Updated upstream
       
 
+=======
+>>>>>>> Stashed changes
 
       if (keyboard.isPressed('a')) {
         this.vx -= this.speed;
@@ -92,10 +98,15 @@ export default class Player {
 
       if (keyboard.isPressed('s')) {
         this.vy += this.speed;
+<<<<<<< Updated upstream
         //this.currState = "active";
       }
       
       
+=======
+      } 
+      
+>>>>>>> Stashed changes
 
       const radian = Math.atan2(mouse.y - (height/2), mouse.x - (width/2));
       let diff = radian - this.radian;
@@ -234,6 +245,7 @@ export default class Player {
       // game.context.fill();
       }
       //Right Torso
+<<<<<<< Updated upstream
       {
       // game.context.fillStyle = '#7F9EA1';
       // game.context.beginPath();
@@ -251,6 +263,18 @@ export default class Player {
       
       //Right Feet
 
+=======
+      game.context.fillStyle = '#7F9EA1';
+      game.context.beginPath();
+      const rt1 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y + 3);
+      const rt2 = rotatePoint(this.x, this.y, this.radian, this.x + 9, this.y + 25);
+      const rt3 = rotatePoint(this.x, this.y, this.radian, this.x - 6, this.y + 25);
+      const rt4 = rotatePoint(this.x, this.y, this.radian, this.x - 10, this.y + 8);
+      game.context.moveTo(rt1.x, rt1.y)
+      game.context.bezierCurveTo( rt2.x, rt2.y, rt3.x, rt3.y, rt4.x, rt4.y)
+      game.context.fill();
+      
+>>>>>>> Stashed changes
       //Draw Head
       {
       game.context.fillStyle = '#4d2600';
@@ -280,10 +304,132 @@ export default class Player {
       game.context.beginPath();
       game.context.arc(right.x, right.y, this.radius * 0.1, 0, 2*Math.PI );
       game.context.fill(); 
+<<<<<<< Updated upstream
       }
       }
 
       if (this.playerState === "active") {
+=======
+      
+      
+
+      if (this.currState === "active") {
+      // Right Arm
+      {
+        game.context.strokeStyle = '#C02942';
+        game.context.beginPath(); 
+        const ra1 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y + 15)
+        game.context.moveTo(ra1.x, ra1.y);
+        const ra2 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y + 13)
+        const ra3 = rotatePoint(this.x, this.y, this.radian, this.x + 20, this.y + 3)
+        const ra4 = rotatePoint(this.x, this.y, this.radian, this.x + 22, this.y - 5)
+        game.context.bezierCurveTo( ra2.x, ra2.y, ra3.x, ra3.y, ra4.x, ra4.y);
+        game.context.moveTo(ra4.x, ra4.y);
+        const ra5 = rotatePoint(this.x, this.y, this.radian, this.x + 20, this.y -8)
+        const ra6 = rotatePoint(this.x, this.y, this.radian, this.x + 18, this.y - 5)
+        game.context.quadraticCurveTo(ra5.x, ra5.y, ra6.x, ra6.y);
+        game.context.moveTo( ra6.x, ra6.y)
+        const ra7 = rotatePoint(this.x, this.y, this.radian, this.x + 9, this.y + 13)
+        const ra8 = rotatePoint(this.x, this.y, this.radian, this.x + 6, this.y + 3)
+        const ra9 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y + 10)
+        game.context.bezierCurveTo( ra7.x, ra7.y, ra8.x, ra8.y, ra9.x, ra9.y);
+        game.context.moveTo(ra1.x, ra1.y);
+        game.context.stroke();
+        }
+        //Right Hand
+        {
+        game.context.fillStyle = '#F1D4AF';
+        game.context.beginPath();
+        const rh = rotatePoint(this.x, this.y , this.radian, this.x + 22, this.y - 6);
+        game.context.arc( rh.x , rh.y, 3, 0, 2*Math.PI );
+        game.context.fill()
+        }
+        //Left Arm
+        {
+        game.context.strokeStyle = '#C02942';
+        game.context.beginPath(); 
+        const la1 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y - 15)
+        game.context.moveTo(la1.x, la1.y);
+        const la2 = rotatePoint(this.x, this.y, this.radian, this.x + 15, this.y - 15)
+        const la3 = rotatePoint(this.x, this.y, this.radian, this.x + 25, this.y - 5)
+        const la4 = rotatePoint(this.x, this.y, this.radian, this.x + 28, this.y + 2)
+        game.context.bezierCurveTo( la2.x, la2.y, la3.x, la3.y, la4.x, la4.y);
+        game.context.moveTo(la4.x, la4.y);
+        const la5 = rotatePoint(this.x, this.y, this.radian, this.x + 25.5, this.y + 5)
+        const la6 = rotatePoint(this.x, this.y, this.radian, this.x + 23, this.y + 2)
+        game.context.quadraticCurveTo(la5.x, la5.y, la6.x, la6.y);
+        game.context.moveTo( la6.x, la6.y)
+        const la7 = rotatePoint(this.x, this.y, this.radian, this.x + 9, this.y - 15)
+        const la8 = rotatePoint(this.x, this.y, this.radian, this.x + 6, this.y - 5)
+        const la9 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y - 10)
+        game.context.bezierCurveTo( la7.x, la7.y, la8.x, la8.y, la9.x, la9.y);
+        game.context.moveTo(la1.x, la1.y);
+        game.context.stroke();
+        }
+        //Left Hand
+        {
+        game.context.fillStyle = '#F1D4AF';
+        game.context.beginPath();
+        const lh = rotatePoint(this.x, this.y , this.radian, this.x + 26, this.y + 2);
+        game.context.arc( lh.x , lh.y, 3, 0, 2*Math.PI );
+        game.context.fill() 
+        }
+        //Left Torso
+        {
+        game.context.fillStyle = '#7F9EA1';
+        game.context.beginPath();
+        const lt1 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y -3);
+        const lt2 = rotatePoint(this.x, this.y, this.radian, this.x + 9, this.y - 25);
+        const lt3 = rotatePoint(this.x, this.y, this.radian, this.x - 6, this.y - 25)
+        const lt4 = rotatePoint(this.x, this.y, this.radian, this.x - 10, this.y - 8)
+        game.context.moveTo(lt1.x, lt1.y)
+        game.context.bezierCurveTo( lt2.x, lt2.y, lt3.x, lt3.y, lt4.x, lt4.y)
+        game.context.fill();
+        }
+        //Right Torso
+        {
+        game.context.fillStyle = '#7F9EA1';
+        game.context.beginPath();
+        const rt1 = rotatePoint(this.x, this.y, this.radian, this.x + 5, this.y + 3);
+        const rt2 = rotatePoint(this.x, this.y, this.radian, this.x + 9, this.y + 25);
+        const rt3 = rotatePoint(this.x, this.y, this.radian, this.x - 6, this.y + 25);
+        const rt4 = rotatePoint(this.x, this.y, this.radian, this.x - 10, this.y + 8);
+        game.context.moveTo(rt1.x, rt1.y)
+        game.context.bezierCurveTo( rt2.x, rt2.y, rt3.x, rt3.y, rt4.x, rt4.y)
+        game.context.fill();
+        }
+        //Draw Head
+        {
+        game.context.fillStyle = '#4d2600';
+        game.context.beginPath();
+        const h1 = rotatePoint(this.x, this.y , this.radian, this.x - 3, this.y );
+        game.context.arc( h1.x , h1.y, this.radius, 0, 2*Math.PI );
+        game.context.fill();
+        }
+        //Draw Face
+        {
+        game.context.fillStyle = '#F1D4AF';
+        game.context.beginPath();
+        const p4 = rotatePoint(this.x, this.y, this.radian, this.x + - 3, this.y);
+        game.context.arc( p4.x, p4.y, this.radius, this.radian - 1.5, this.radian + 1.5 );
+        game.context.fill();
+        }
+        //Draw Eyes
+        {
+        game.context.fillStyle = '#000';
+        const left = rotatePoint(this.x, this.y, this.radian, this.x + 2, this.y - this.radius * 0.3);
+        game.context.beginPath();
+        game.context.arc( left.x, left.y, this.radius * 0.1, 0, 2*Math.PI );
+        game.context.fill();
+  
+        game.context.fillStyle = '#000';
+        const right = rotatePoint(this.x, this.y, this.radian, this.x + 2, this.y + this.radius * 0.3);
+        game.context.beginPath();
+        game.context.arc(right.x, right.y, this.radius * 0.1, 0, 2*Math.PI );
+        game.context.fill(); 
+        }
+
+>>>>>>> Stashed changes
 
       }
       //DEBUG
